646c61b33f36be19a6b7edfa56bdd8f9ec653caa
Arty <kirya26763@mail.ru>
2024-11-20 12:40:40 +0700

    init

--- src/Car.java
+++ src/Car.java
@@ -1,13 +1,16 @@
-public class Car {
+public class Car extends Transport {
 
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
 
     public void checkEngine() {
         System.out.println("Проверяем двигатель");
+    }
+
+    @Override
+    public void service() {
+        super.service();
+        checkEngine();
     }
 }
--- src/Truck.java
+++ src/Truck.java
@@ -1,17 +1,16 @@
-public class Truck {
+public class Truck extends Transport {
 
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
 
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
+    }
+
+    @Override
+    public void service() {
+        super.service();
+        checkTrailer();
     }
 }
